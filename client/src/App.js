2a00a3817c6fea326cd92e1839e67b05e423fb86
Abhishek <abhishekpatel46420@gmail.com>
2024-09-13 10:05:57 +0530

    home routing in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing/LandingPage';
 import CommunitySupport from './pages/CommunitySupport/CommunitySupport';
 import Login from './pages/AuthPages/Login';
+import HomePage from './pages/homePage/HomePage';
+
 import './App.css';
 
 
@@ -9,6 +11,7 @@ function App() {
   return (
     <Routes>
       <Route exact path="/" element={<LandingPage />} />
+      <Route exact path ="/home" element ={<HomePage />} />
     </Routes>
   );
 }
